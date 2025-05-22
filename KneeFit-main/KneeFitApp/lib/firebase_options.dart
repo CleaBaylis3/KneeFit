fee077dfb126106c6035b2649365c7325954ca0c
CBaylis3 <cleabaylis@icloud.com>
2025-05-22 12:03:09 -0600

    Configure Firebase SDK with google-services plugin and fix images

--- KneeFit-main/KneeFitApp/lib/firebase_options.dart
+++ KneeFit-main/KneeFitApp/lib/firebase_options.dart
@@ -1,14 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
 
+// class DefaultFirebaseOptions {
+//   static FirebaseOptions get currentPlatform {
+//     return FirebaseOptions(
+//       apiKey: "AIzaSyDgJr_4YgBnx2vlUZrVsVxkB2drhXnl6_4",
+//       authDomain: "kneefit-app.firebaseapp.com",
+//       projectId: "kneefit-app",
+//       storageBucket: "kneefit-app.appspot.com",
+//       messagingSenderId: "387804831222",
+//       appId: "1:387804831222:android:0ec0c5a50f39d37541fbd7",
+//     );
+//   }
+// }
+
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return FirebaseOptions(
-      apiKey: "AIzaSyDgJr_4YgBnx2vlUZrVsVxkB2drhXnl6_4",
-      authDomain: "kneefit-app.firebaseapp.com",
-      projectId: "kneefit-app",
-      storageBucket: "kneefit-app.appspot.com",
-      messagingSenderId: "387804831222",
-      appId: "1:387804831222:android:0ec0c5a50f39d37541fbd7",
+      apiKey: "AIzaSyAnOuN3IK8n0Dt-5MhV1ADfg12lhKRrtF4",
+      authDomain: "esp32-frebase-demo-63bff.firebaseapp.com",
+      projectId: "esp32-frebase-demo-63bff",
+      storageBucket: "esp32-frebase-demo-63bff.appspot.com",
+      messagingSenderId: "928148205346",
+      appId: "1:928148205346:android:9b728e05a498467517ff7b",
     );
   }
 }
+
